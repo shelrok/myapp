9ccc0c505394a61ab2af9e2a73005a001525e7d2
Your Name <you@example.com>
2024-11-22 21:40:50 +0300

    change concept

--- app/app.py
+++ app/app.py
@@ -1,33 +1,25 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+import os
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+# Путь к папке шаблонов и статических файлов
+TEMPLATES_FOLDER = os.path.join(os.path.dirname(__file__), 'backend', 'templates')
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'backend', 'static')
 
-@app.route("/")
-def hello_world():
+app = Flask(__name__, template_folder=TEMPLATES_FOLDER, static_folder=STATIC_FOLDER)
 
-    return "<p>Hello, World!</p>"
-if __name__ == 'main':
-  app.run(debug=True)
+# Папка, где будут храниться аудиофайлы
+AUDIO_FOLDER = os.path.join(STATIC_FOLDER, 'audio')
 
-@app.route('/about')
-def about():
-  return 'This is the about page'
+# Главная страница с аудиофайлами
+@app.route('/')
+def index():
+    audio_files = os.listdir(AUDIO_FOLDER)
+    return render_template('index.html', audio_files=audio_files)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-  return f'User {username}'
+# Эндпоинт для проигрывания аудио
+@app.route('/audio/<filename>')
+def get_audio(filename):
+    return send_from_directory(AUDIO_FOLDER, filename)
 
-@app.route('/hello/<name>')
-def hello(name):
-  return render_template('hello.html', name=name)
-
-@app.route('/greet/<name>')
-def greet(name):
-  return f'Hello, {name}!'
-
-@app.route('/submit', methods=['POST'])
-def submit():
-  name = request.form['name']
-  return f'Hello, {name}'
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
