6f7fc798d26cce3b81260ee7bf818258ef6dfd94
lotolis <lotolis@mail.ru>
2025-03-22 17:17:25 +0300

    новые стили

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/backend/**/*.html",   // Путь к вашим HTML файлам
-    "./app/backend/**/*.css",    // Путь к вашим CSS файлам, если в них используются Tailwind классы
+    './app/backend/templates/**/*.{html,jinja}',
+    './app/backend/static/**/*.{html,js}',
   ],
   theme: {
     extend: {},
